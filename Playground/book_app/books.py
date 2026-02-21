6561b19dc6208f1e838f3a67c99150002ab139bd
Rahul Dey <rahuldey9623@gmail.com>
2026-02-21 08:17:34 +0000

    Added search by category using query parameter

--- Playground/book_app/books.py
+++ Playground/book_app/books.py
@@ -1,12 +1,14 @@
 import logging
+from typing import Optional
+from pydantic import BaseModel
 
 from fastapi import FastAPI, status, HTTPException
-from pydantic import BaseModel
 
 app = FastAPI()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 class Book(BaseModel):
     id: int
@@ -21,20 +23,31 @@ BOOKS: list[Book] = [
         id=2,
         title="The Gifts of Imperfection",
         author="Brené Brown",
-        category="Self-Help",
+        category="Motivational",
     ),
     Book(
         id=3, title="The Mountain Is You", author="Brianna Wiest", category="Self-Help"
     ),
-    Book(id=4, title="The Daily Stoic", author="Ryan Holiday", category="Self-Help"),
-    Book(id=5, title="Start With Why", author="Simon Sinek", category="Self-Help"),
+    Book(id=4, title="The Daily Stoic", author="Ryan Holiday", category="Motivational"),
+    Book(id=5, title="Start With Why", author="Simon Sinek", category="Logical"),
 ]
 
 
 @app.get("/books", status_code=status.HTTP_200_OK, response_model=list[Book])
-def get_all_books() -> list[Book]:
-    """Retrieve all books from database"""
-    return BOOKS
+def get_all_books(category: Optional[str] = None) -> list[Book]:
+    """Retrieve all books from database optionally filter by category"""
+    logger.info("Retrieving books from database...")
+
+    if category is None:
+        logger.info(f"Books retrieved successfully. Count: {len(BOOKS)}")
+        return BOOKS
+
+    logger.info(f"Filtering books by category: {category}...")
+    filter_books: list[Book] = [
+        b for b in BOOKS if b.category.casefold() == category.casefold()
+    ]
+    logger.info(f"Found filter books. Count: {len(filter_books)}")
+    return filter_books
 
 
 @app.get("/books/{title:str}", status_code=status.HTTP_200_OK, response_model=Book)
